41de78c6a439173ac916a8647a0131ac24eea911
michal-webskie <michal@webskie.pl>
2023-12-04 15:20:08 +0100

    final version of the script

--- main.js
+++ main.js
@@ -3,39 +3,61 @@ const input = document.querySelector('input')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
 
+const answers = [
+	'Nie',
+	'Tak',
+	'Zapomnij',
+	'Nigdy w życiu!',
+	'No jacha!',
+	'Dasz radę',
+	'Jazda z tematem!',
+	'Dalej, dalej ręce Gadgeta!',
+	'Nie dla nas kwitnie ananas!',
+	'Za wysokie progi jak na Twoje nogi!',
+	'Myśl pozytywnie',
+	'Ale z Ciebie debil!',
+	'Łatwizna!',
+	'Easy!',
+	'Go, go PowerRangers!',
+	'Nie myśl tylko działaj!',
+	'JUST DO IT',
+	'Za dużo rozkminiasz!',
+]
+
 const addShake = () => {
 	ball.classList.add('shake-animaton')
 	setTimeout(removeShake, 1100)
+
+	askQuestion()
 }
 const removeShake = () => {
 	ball.classList.remove('shake-animaton')
 }
 
-// const askAquestion = () => {
-//     const question = input.value
-
-//     if (question !== '') {
-//         console.log("question !== ''");
-//     } else if (question !== '' && ) {
-
-//     }
-// }
-
-const test = () => {
+const askQuestion = () => {
 	const question = input.value
 
 	if (question === '') {
-		error.textContent = 'Wpisz swoje pytanie'
+		error.innerHTML = 'Wpisz swoje pytanie'
+		answer.textContent = ''
 	} else if (question.indexOf('?') !== question.length - 1) {
 		error.innerHTML = 'Twierdzisz czy pytasz? <br> Jeżeli to drugie to postaw pytajnik na końcu.'
+		answer.textContent = ''
+	} else if (question !== '' && question.indexOf('?') === question.length - 1) {
+		error.innerHTML = ''
+		setTimeout(randomAnswer, 1200)
 	}
+}
 
-	console.log(question.length)
+const randomAnswer = () => {
+	const randomAnswerNr = Math.floor(Math.random() * answers.length)
+	answer.textContent = answers[randomAnswerNr]
 }
 
 const enterCheck = e => {
 	if (e.key === 'Enter') {
-		test()
+		askQuestion()
+		addShake()
 	}
 }
 
